69bfd04cb97886a7998f5f1ea33fbc32eb35d4c7
William4312 <gunawanandreas02@gmail.com>
2023-12-21 18:57:54 +0700

    menambah komponent ke collectoral_factor

--- collectoral_factor.py
+++ collectoral_factor.py
@@ -1,88 +1,63 @@
 from manim import *
 
-from manim import *
-
-class CollateralLoan(Scene):
-    def construct(self):
-        # Create the logo and the text
-        logo = Text("R RareSkill").scale(0.2).to_corner(UR)
-        title = Text("Collateral Loan", font_size=48).next_to(logo, DOWN, buff=0.5)
-        self.add(logo, title)
-
-        # Create the bars and the labels
-        collateral = Rectangle(width=6, height=1, color=WHITE, fill_opacity=1).next_to(title, DOWN, buff=1)
-        loan = Rectangle(width=4.5, height=1, color=WHITE, fill_opacity=1).next_to(collateral, DOWN, buff=0.5)
-        collateral_label = MathTex("Collateral", color=BLACK).next_to(collateral, UP, buff=0.1)
-        loan_label = MathTex("Loan", color=BLACK).next_to(loan, UP, buff=0.1)
-        
-        self.play(
-            Create(collateral),
-            Create(loan),
-            Write(collateral_label),
-            Write(loan_label)
-        )
-
-        # Create the factor and the equation
-        factor = DecimalNumber(0.9, num_decimal_places=2, color=WHITE).next_to(loan, RIGHT, buff=1)
-        factor_label = MathTex("\\text{Collateral Factor}", color=WHITE).next_to(factor, DOWN, buff=0.1)
-        equation = MathTex("\\text{Loan} = \\text{Collateral} \\times \\text{Collateral Factor}", color=WHITE).next_to(factor, RIGHT, buff=1)
-        self.play(
-            Write(factor),
-            Write(factor_label),
-            Write(equation)
-        )
-
-        # Animate the change of the factor and the loan
-        self.play(
-            factor.animate.set_value(0.8),
-            loan.animate.stretch_to_fit_width(3.6),
-            run_time=2
-        )
-        self.wait()
-        
 class CollateralLoanScene(Scene):
     def construct(self):
         # Create text labels
-        collateral_label = Text("Collateral", color=WHITE).to_edge(UP)
+        collateral_label = Text("Collateral", color=WHITE).next_to(UP*2+LEFT*7)
         loan_label = Text("Loan", color=WHITE).next_to(collateral_label, DOWN, buff=1.5)
+        loan_label.align_to(collateral_label, LEFT)
 
         # Create the bars
-        collateral_bar = Rectangle(width=5, height=0.5, color=WHITE).next_to(collateral_label, DOWN)
-        loan_bar = Rectangle(width=4.5, height=0.5, color=WHITE).next_to(loan_label, DOWN)
+        original_width = 8
+        collateral_bar = RoundedRectangle(width=original_width, height=1, color=WHITE, corner_radius=0.2).next_to(collateral_label, RIGHT)
+        loan_bar = RoundedRectangle(width=original_width, height=1, color=WHITE, corner_radius=0.2).next_to(loan_label, RIGHT)
+        loan_bar.align_to(collateral_bar, LEFT)
 
         # Create the percentage text
-        percentage_label = Text("Collateral_Factor = 90%", color=WHITE).to_edge(DOWN)
-
+        factor = Text("100")
+        percentage_label = Text("Collateral_Factor =", color=WHITE)
+        tempFactor = VGroup(percentage_label, factor, Text("%")).arrange(direction=RIGHT, buff=0.3).next_to(LEFT*8 + DOWN*3.1).scale(0.75)
+        
         # Create the brand text
-        brand_label = Text("RareSkills", color=WHITE).next_to(percentage_label, DOWN)
+        brand_label = ImageMobject("media\images/collectoral_factor/rareskillLogo.png").next_to(RIGHT*3+DOWN*3.1)
 
         # Group everything together
-        self.add(collateral_label, loan_label, collateral_bar, loan_bar, percentage_label, brand_label)
+        self.add(collateral_label, loan_label, collateral_bar, loan_bar, tempFactor, brand_label)
+        
+        # Calculate the shift to the left ne/ded during stretching
+        new_width70 = 8*0.7
+        shift_amount70 = (loan_bar.width - new_width70) / 2
 
-        # Animate the bars
-        self.play(
-            collateral_bar.animate.set_width(9, stretch=True),
-            loan_bar.animate.set_width(8.1, stretch=True),
-            run_time=2
-        )
+        self.play(loan_bar.animate.stretch_to_fit_width(new_width70).shift(LEFT * shift_amount70))
+       
+        for i in range(100, 69, -1):
+            self.play(Transform(tempFactor[1], Tex(f"{i}%").next_to(tempFactor[0], RIGHT)), run_time=0.1, rate_func=linear)
+            
+        brace_70 = BraceBetweenPoints(
+            loan_bar.get_right(), 
+            loan_bar.get_right() + RIGHT * (original_width - new_width70), 
+            direction=DOWN, 
+            color=GREEN
+        ).shift(DOWN * 0.2)  # Adjust the 0.2 value as needed
+        brace_text70 = brace_70.get_text(r"Higher Margin \\of Safety")
+        self.play(GrowFromCenter(brace_70), Write(brace_text70))
+        self.play(FadeOut(brace_70, brace_text70))
+        
+        new_width90 = 8 * 0.9
+        shift_amount90 = (loan_bar.width - new_width90) / 2
+        self.play(loan_bar.animate.stretch_to_fit_width(new_width90).shift(LEFT * shift_amount90))
+        
+        for i in range(70, 91, 1):
+            self.play(Transform(tempFactor[1], Tex(f"{i}%").next_to(tempFactor[0], RIGHT)), run_time=0.1, rate_func=linear)
+       
+        # Create a brace for the 90% stretch
+        brace_90 = BraceBetweenPoints(
+            loan_bar.get_right(), 
+            loan_bar.get_right() + RIGHT * (original_width - new_width90), 
+            direction=DOWN, 
+            color=RED
+        ).shift(DOWN * 0.2)
+        brace_text90 = brace_90.get_text(r"Lower Margin \\of Safety")
 
-        # Hold the final state for a brief moment
-        self.wait(2)
-
-class QuickPercentDecreaseScene(Scene):
-    def construct(self):
-        # Create a Tex object to display the initial percentage
-        percentage = Tex("100%").scale(2)
-        self.add(percentage)
-
-        # Animate the percentage decreasing from 100% to 60%
-        for i in range(100, 59, -1):
-            self.play(Transform(percentage, Tex(f"{i}%").scale(2)), run_time=0.1, rate_func=linear)
-
-        # Hold the final state for a brief moment
-        self.wait(1)
-
-# To render the scene, use the following command in the terminal:
-# manim -pql your_script_name.py QuickPercentDecreaseScene
-
-    
+        self.play(GrowFromCenter(brace_90), Write(brace_text90))
+        self.play(FadeOut(brace_90, brace_text90))
